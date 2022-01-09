f5b486c10fe14de672c1be995adf508a5884ed76
SP1N4X <82057630+SP1N4X@users.noreply.github.com>
2022-01-09 15:07:26 +0100

    Update get.py

--- get.py
+++ get.py
@@ -6,12 +6,12 @@ try:
     #next_match = '15:30'
     #next_match = datetime.strptime(next_match, '%H:%M') - timedelta(minutes=1.5)
     #print(next_match)
-    host = 'https://www.google.it'
-    headers = {"Referer": "https://www.betexplorer.com",
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'}
+    host = 'https://www.sisal.it'
+    url_alberatura = '/api-betting/vrol-api/vrol/palinsesto/getAlberaturaEventiSingoli/1'
+    headers = {'User-Agent':'Mozilla/5.0'}
     s = requests.Session()
     session = s.headers.update(headers)
-    request = s.get(host)
+    request = s.get(host+url_alberatura)
     response = request.status_code
     print(response)
 except Exception as e:
