aa6aa11001ff6fd91fc81e20c6da4c9849de6e3a
SP1N4X <82057630+SP1N4X@users.noreply.github.com>
2022-02-04 15:56:34 +0100

    Update cal_result.py

--- cal_result.py
+++ cal_result.py
@@ -3,11 +3,11 @@ from xgboost import XGBClassifier
 
 def cal(data):
   print(data)
-  xnew = pd.DataFrame.from_dict(data)
-  xnew = xnew.drop(['orario'],1).drop(['ospite'],1).drop(['casa'],1)
+  x = pd.DataFrame.from_dict(data)
+  xnew = x.drop(['orario'],1).drop(['ospite'],1).drop(['casa'],1)
   model = XGBClassifier()
   model.load_model("model.txt")
   ynew1 = str(model.predict(xnew)[-1])
   ynew2 = str(model.predict_proba(xnew)[-1])
-  result = ynew1 + ynew2
+  result = {'casa':x['casa'],'ospite':x['ospite'],'orario':x['orario'],'pred':xnew1,'prob_1':ynew2[1],'prob_X':ynew2[0],'prob_2':ynew2[2]}
   return result
