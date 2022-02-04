b207ac56c431d295518354a235f94a0fc85cd7c0
SP1N4X <82057630+SP1N4X@users.noreply.github.com>
2022-02-04 16:19:46 +0100

    Update cal_result.py

--- cal_result.py
+++ cal_result.py
@@ -8,6 +8,6 @@ def cal(data):
   model = XGBClassifier()
   model.load_model("model.txt")
   ynew1 = str(model.predict(xnew)[-1])
-  ynew2 = str(model.predict_proba(xnew)[-1])
-  result = {'casa':x['casa'],'ospite':x['ospite'],'orario':x['orario'],'pred':ynew1,'prob_1':ynew2[1],'prob_X':ynew2[0],'prob_2':ynew2[2]}
+  ynew2 = model.predict_proba(xnew)[-1]
+  result = {'casa':x['casa'].iloc[0],'ospite':x['ospite'].iloc[0],'orario':x['orario'].iloc[0],'pred':ynew1,'prob_1':ynew2[1],'prob_X':ynew2[0],'prob_2':ynew2[2]}
   return result
