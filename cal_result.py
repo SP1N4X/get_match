bdbc647d3a24709f04608903e976359315e26e6c
SP1N4X <82057630+SP1N4X@users.noreply.github.com>
2022-02-04 15:24:19 +0100

    Update cal_result.py

--- cal_result.py
+++ cal_result.py
@@ -2,6 +2,7 @@ import pandas as pd
 from xgboost import XGBClassifier
 
 def cal(data):
+  print(data)
   xnew = pd.DataFrame.from_dict(data)
   xnew = xnew.drop(['orario'],1).drop(['ospite'],1).drop(['casa'],1)
   model = XGBClassifier()
